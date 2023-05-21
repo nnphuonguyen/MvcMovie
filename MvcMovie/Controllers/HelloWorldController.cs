aa372ba9cad89cb7e778a761c63365136045645f
PHUONG UYEN <PHUONG UYEN@LAPTOP-AO3F4K1D>
2023-05-21 19:56:45 +0700

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+     public IActionResult Welcome(string name, int numTimes = 1)
+       {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+       }
+        //public string Index()
+        //{
+          //  return " This is my fault action...";
+        //
+        //}
+
+        //GET /HelloWorld/Welcome
+        //Requirea using system.Text.Encoding.Web
+       // public string Welcome(string name, int ID = 1)
+       // {
+           // return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+       // }
+        
+    }
+}
